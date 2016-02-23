b5b2893ba5b23d83c349ca970ec6e7f49ad32cc0
MGRiv <mgrivera501@gmail.com>
2016-02-23 13:07:50 -0500

    colors

--- hw02/pulse.js
+++ hw02/pulse.js
@@ -4,12 +4,23 @@ var ctx = c.getContext("2d");
 var diff = -1;
 var rad = 0;
 
-ctx.fillStyle = "#dd0000";
+ctx.fillStyle = "#ff0000";
+var col = 1;
 
 var pulse = function pulse(e) {
     ctx.clearRect(0, 0, c.width, c.height);
     if(rad == c.width/2 || rad == 0){
 	diff = diff * -1;
+	if(col == 1){
+	    ctx.fillStyle = "#00ff00";
+	    col = 2;
+	}else if(col == 2){
+	    ctx.fillStyle = "#0000ff";
+	    col = 3;
+	}else{
+	    ctx.fillStyle = "#ff0000";
+	    col = 1;
+	}
     }
     rad = rad + diff;
     ctx.beginPath();
