1ba7356554859a9a2b0637adc916ee0a8af54366
MGRiv <mgrivera501@gmail.com>
2016-02-25 00:23:44 -0500

    bouncy

--- hw03/bounce.js
+++ hw03/bounce.js
@@ -2,24 +2,41 @@ var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 var b = document.getElementById("button1");
 var s = document.getElementById("button2");
-var xdiff = 1;
-var posx = c.width/2;
-var posy = c.height/2;
-    
-var ydiff = 1;
 var frameid;
-var img;
+var img = new Image();
+img.src = "ball.png";
 
-b.addEventListener("click",bounce);
-window.onload = function() {
-    img = new Image();
-    img.src = "ball.png"
-    ctx.drawImage(img,c.width/2,c.height/2,25,25);
-}
+
 
 var bounce = function bounce(e) {
-    ctx.clearRect(0,0,c.width,c.height);
-    if(
-    ctx.drawImage(img,posx,posy,25,25);
-    if
+    window.cancelAnimationFrame(frameid);
+    var posx = Math.floor(Math.random() * 300) + 100;
+    var posy = Math.floor(Math.random() * 300) + 100;
+    var xdiff = 1;    
+    var ydiff = 0;
+    var mod = .05;
+    var move = function (e) {
+	ctx.clearRect(0,0,500,500);
+	posx += xdiff;
+	posy += ydiff;
+	if(posx < 0 || posx > 460){
+	    posx -= xdiff;
+	    xdiff *= -1;
+	}
+	if(posy > 460){
+	    posy -= ydiff;
+	    ydiff *= -1;
+	}
+	ydiff += mod;
+	ctx.drawImage(img,posx,posy,40,40);
+	frameid = window.requestAnimationFrame(move);
+    };
+    move();
+};
+
+var cease = function cease(e) {
+    window.cancelAnimationFrame(frameid);
 }
+
+b.addEventListener("click",bounce);
+s.addEventListener("click",cease);
